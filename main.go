1c6b80f8d7776b7e0a9840eacc01ab4c7e703239
Courtney Ruhm <cruhm@cruhm.remote.csb>
2023-03-14 12:11:04 -0600

    implement walk function

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+type file struct {
+	path string
+	name string
+}
+
+func main() {
+	dir := "./sample"
+	var toRename []file
+	filepath.Walk(dir, func(path string, info fs.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+		if _, err := match(info.Name()); err == nil {
+			toRename = append(toRename, file{
+				name: info.Name(),
+				path: path,
+			})
+		}
+		return nil
+	})
+	for _, f := range toRename {
+		fmt.Printf("%q\n", f)
+	}
+	for _, orig := range toRename {
+		var n file
+		var err error
+		n.name, err = match(orig.name)
+		if err != nil {
+			fmt.Println("Error matching:", orig.path, err.Error())
+		}
+		if err != nil {
+			panic(err)
+		}
+		n.path = filepath.Join(dir, n.name)
+		fmt.Printf("mv %s => %s\n", orig.path, n.path)
+		err = os.Rename(orig.path, n.path)
+		if err != nil {
+			fmt.Println("Error naming:", orig.path, err.Error())
+		}
+	}
+
+}
+
+func match(filename string) (string, error) {
+	pieces := strings.Split(filename, ".")
+	ext := pieces[len(pieces)-1]
+	tmp := strings.Join(pieces[0:len(pieces)-1], ".")
+	pieces = strings.Split(tmp, "_")
+	name := strings.Join(pieces[0:len(pieces)-1], "_")
+	number, err := strconv.Atoi(pieces[len(pieces)-1])
+	if err != nil {
+		return "", fmt.Errorf("%s didn't match our pattern", filename)
+	}
+	return fmt.Sprintf("%s - %d.%s", strings.Title(name), number, ext), nil
+}
